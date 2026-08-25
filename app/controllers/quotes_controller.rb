2fea51ab2ed36f559de6624d019e9e8676d253f3
Benjamin BOCCARA <boccarab@gmail.com>
2026-08-25 10:27:06 +0200

    Add QuoteController and routes

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,66 @@
+class QuotesController < ApplicationController
+  before_action :set_quote, only: %i[show edit update destroy publish archive]
+
+  def index
+    @quotes = Quote.all
+  end
+
+  def show
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def edit
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to @quote, notice: "Quote was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @quote.update(quote_params)
+      redirect_to @quote, notice: "Quote was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @quote.destroy!
+    redirect_to quotes_path, notice: "Quote was successfully destroyed.", status: :see_other
+  end
+
+  def publish
+    update_status(:published, "Quote was successfully published.")
+  end
+
+  def archive
+    update_status(:archived, "Quote was successfully archived.")
+  end
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+
+  def quote_params
+    params.expect(quote: %i[name identifier])
+  end
+
+  def update_status(status, notice)
+    if @quote.update(status: status)
+      redirect_to @quote, notice: notice
+    else
+      redirect_to @quote, alert: @quote.errors.full_messages.to_sentence
+    end
+  end
+end
