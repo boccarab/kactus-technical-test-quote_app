182e4fc526a475335efcfd7f4a61364cc4248cfd
Benjamin BOCCARA <boccarab@gmail.com>
2026-08-25 10:08:49 +0200

    Create quote model
    DB table
    Define status enum
    Generate identifier on create
    Prevent invalid status change
    Test coverage

---
+++ app/models/quote.rb
@@ -0,0 +1,31 @@
+class Quote < ApplicationRecord
+    enum :status, %i[draft published archived].index_by(&:itself), prefix: true, default: :draft
+
+    before_create :generate_identifier, if: -> { identifier.blank? }
+    before_update :prevent_status_update, if: -> {
+        will_save_change_to_status? && %w[published archived].include?(status_in_database)
+    }
+
+    def name
+        self[:name].presence || "sans titre"
+    end
+
+    private
+
+    def generate_identifier
+        year = Time.current.year
+        next_id = Quote.count + 1
+
+        self.identifier = loop do
+            value = "d-#{year}-#{next_id}" # default format. We might allow user to have custom format l
+            break value unless self.class.exists?(identifier: value)
+
+            next_id += 1
+        end
+    end
+
+    def prevent_status_update
+        errors.add(:status, "cannot be changed after publication or archiving")
+        throw :abort
+    end
+end
